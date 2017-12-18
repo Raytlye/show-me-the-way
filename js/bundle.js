bd824f0f1494791819e2d22fffc0992d804cd4e5
lvonnied <lvonnied@hsr.ch>
2017-12-18 09:16:02 +0100

    Replaced Layer with Google Maps

--- js/bundle.js
+++ js/bundle.js
@@ -66,7 +66,12 @@ if (filteredBbox) {
     overview_map.setView(mapCenter, 4);
 }
 
-var bing = new L.BingLayer(BING_KEY, 'Aerial').addTo(map);
+//var bing = new L.BingLayer(BING_KEY, 'Aerial').addTo(map);
+
+var bing = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+  subdomains:['mt0','mt1','mt2','mt3'],
+  opacity: 0.5
+}).addTo(map);
 
 var osm = new L.TileLayer('https://api.mapbox.com/styles/v1/openstreetmapus/cj8xtgojqhd3z2sorzpi01csj/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib3BlbnN0cmVldG1hcHVzIiwiYSI6ImNpc2x0eGF1MjBhZTIydXB1eTkxbTdrdXoifQ.9reDkEfppIvAOfAD3tRDJQ', {
     minZoom: 4,
